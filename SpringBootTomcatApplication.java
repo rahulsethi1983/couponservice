63fbe4a18c3f572e0a6a9196722a648d375562c7
rahulsethi1983 <rahul.rahulsethi1983@gmail.com>
2022-11-27 13:14:10 +0530

    Add files via upload

---
+++ SpringBootTomcatApplication.java
@@ -0,0 +1,10 @@
+package com.tesco.coupon;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootTomcatApplication extends SpringBootServletInitializer {
+
+
+}
